7c7b29a3277159ddbafcf0127930f28abb0b6349
Lucas <luks2k19@gmail.com>
2022-01-31 09:45:15 -0300

    new cambio

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,9 @@
 /*
+Lucas Soriano Baccarelli DIV E
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Esto funciona de maravilla"); 
+	alert("Esto funciona de maravilla");
 }
 
