824740ea93bca877d2b6d8af53a76d642f565399
Lucas <luks2k19@gmail.com>
2022-01-31 12:28:21 -0300

    up

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -1,9 +1,24 @@
 /*
+Lucas Soriano Baccarelli DIV E
+E/S Eje6
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	//declarar var
+	var numeroUno;
+	var numeroDos;
+	var sumaTotal;
+
+	//tomar los datos de las casillas
+	numeroUno = document.getElementById("txtIdNumeroUno").value;
+	numeroDos = document.getElementById("txtIdNumeroDos").value;
+
+	//sumar las dos var
+	sumaTotal = parseInt(numeroUno) + parseInt(numeroDos);
+
+	//mostrar el resultado
+	alert("El resultado es: " + sumaTotal);
 }
 
