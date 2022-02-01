9e8635525fb7ed7babe9f8ebe0aa4bb927404675
Lucas <luks2k19@gmail.com>
2022-02-01 08:43:08 -0300

    new

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -1,25 +1,78 @@
 /*
+Lucas Soriano Baccarelli DIV E
+E/S Eje7
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	//declarar var
+	var numeroUno;
+	var numeroDos;
+	var sumaTotal;
+
+	//tomar los datos de las casillas
+	numeroUno = document.getElementById("txtIdNumeroUno").value;
+	numeroDos = document.getElementById("txtIdNumeroDos").value;
+
+	//sumar las dos var
+	sumaTotal = parseInt(numeroUno) + parseInt(numeroDos);
+
+	//mostrar el resultado
+	alert("El resultado es: " + sumaTotal);	
 }
 
 function restar()
 {
-	
+	//declarar var
+	var numeroUno;
+	var numeroDos;
+	var restaTotal;
+
+	//tomar los datos de las casillas
+	numeroUno = document.getElementById("txtIdNumeroUno").value;
+	numeroDos = document.getElementById("txtIdNumeroDos").value;
+
+	//sumar las dos var
+	restaTotal = parseInt(numeroUno) - parseInt(numeroDos);
+
+	//mostrar el resultado
+	alert("El resultado es: " + restaTotal);
 }
 
 function multiplicar()
 { 
-	
+	//declarar var
+	var numeroUno;
+	var numeroDos;
+	var multiTotal;
+
+	//tomar los datos de las casillas
+	numeroUno = document.getElementById("txtIdNumeroUno").value;
+	numeroDos = document.getElementById("txtIdNumeroDos").value;
+
+	//sumar las dos var
+	multiTotal = parseInt(numeroUno) * parseInt(numeroDos);
+
+	//mostrar el resultado
+	alert("El resultado es: " + multiTotal);
 }
 
 function dividir()
 {
-	
-}
+	//declarar var
+	var numeroUno;
+	var numeroDos;
+	var diviTotal;
 
+	//tomar los datos de las casillas
+	numeroUno = document.getElementById("txtIdNumeroUno").value;
+	numeroDos = document.getElementById("txtIdNumeroDos").value;
+
+	//sumar las dos var
+	diviTotal = parseInt(numeroUno) / parseInt(numeroDos);
+
+	//mostrar el resultado
+	alert("El resultado es: " + diviTotal);
+}
